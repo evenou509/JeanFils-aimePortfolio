cedae442f1f7a6c1e455697ac57987cd30aa8e83
Jean Fils-Aime <jeaneddy25@gmail.com>
2019-03-13 06:25:41 -0400

    Updates

---
+++ precache-manifest.83b89c04e911fbb89868614ed71594f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0d3526171f02cb30485",
+    "url": "/JeanFils-aimePortfolio/static/js/runtime~main.efac65cb.js"
+  },
+  {
+    "revision": "00b27de601d1288afb68",
+    "url": "/JeanFils-aimePortfolio/static/js/main.0baafb82.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/JeanFils-aimePortfolio/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "00b27de601d1288afb68",
+    "url": "/JeanFils-aimePortfolio/static/css/main.4d229d6b.chunk.css"
+  },
+  {
+    "revision": "71bc4657593037b41abaff115d4d949e",
+    "url": "/JeanFils-aimePortfolio/index.html"
+  }
+];
