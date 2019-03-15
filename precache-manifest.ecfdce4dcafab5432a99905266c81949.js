c8325ce9c12346c57af3ad648adccc462ab60c31
Jean Fils-Aime <jeaneddy25@gmail.com>
2019-03-15 19:17:01 -0400

    Updates

---
+++ precache-manifest.ecfdce4dcafab5432a99905266c81949.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0d3526171f02cb30485",
+    "url": "/JeanFils-aimePortfolio/static/js/runtime~main.efac65cb.js"
+  },
+  {
+    "revision": "ad3fc018d701e9ff5aec",
+    "url": "/JeanFils-aimePortfolio/static/js/main.9f03039b.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/JeanFils-aimePortfolio/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "ad3fc018d701e9ff5aec",
+    "url": "/JeanFils-aimePortfolio/static/css/main.4d229d6b.chunk.css"
+  },
+  {
+    "revision": "6182f42cdb6e6cbb84310e7c3138f035",
+    "url": "/JeanFils-aimePortfolio/index.html"
+  }
+];
--- src/components/resume/resumeData.js
+++
@@ -1,6 +0,0 @@
-let resumeData = {
-    "imagebaseurl":"http://localhost:3000/",
-    "name": "Jean Fils-aime",
-    "role": "Frontend Developer"
-    }
-  export default resumeData
